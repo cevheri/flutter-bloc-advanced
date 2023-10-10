cc48c7c0c2a53486204c4a5e545e4128e37deb98
cevheri <cevheribozoglan@gmail.com>
2023-10-10 23:30:54 +0300

    add constant class

---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+class AppConstant{
+  static const String APP_NAME = "User-Task-Management";
+  static const String APP_VERSION = "1.0.0";
+  static const String APP_DESCRIPTION = "User Roles and Task Management mobile and web application";
+  static const String APP_AUTHOR = "Yusuf";
+  static const String APP_AUTHOR_EMAIL = "yusufgundogdu555@gmail.com";
+}
