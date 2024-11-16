6f73e9dd9f4424ae834fae497e7f144af3bb8b28
ucmazmehmet <mucmaz1@gmail.com>
2024-11-16 14:49:23 +0300

    test: add district test and fake data

--- test/fake/city_data.dart
+++ test/fake/city_data.dart
@@ -1,7 +1,16 @@
 import 'package:flutter_bloc_advance/data/models/city.dart';
+import 'package:flutter_bloc_advance/data/models/district.dart';
 
+/// City Fake data with full payload
 final cityMockPayload = City(
   id: 1,
   name: 'istanbul',
   plateCode: '34',
+);
+
+/// District Fake data with full payload
+final districtMockPayload = District(
+  id: '1',
+  name: 'istanbul',
+  code: '01',
 );
