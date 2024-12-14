bafe6bed925efd0a106b1ab5956625b52c9b2410
cevheri <cevheribozoglan@gmail.com>
2024-12-14 03:18:53 +0300

    fix: go_router redirect to login when account is failure(invalid TOKEN)

--- lib/routes/go_router_routes/app_go_router_config.dart
+++ lib/routes/go_router_routes/app_go_router_config.dart
@@ -56,22 +56,29 @@ class AppGoRouterConfig {
       _log.debug("BEGIN: redirect");
       _log.debug("redirect - uri: ${state.uri}");
       final accountBloc = context.read<AccountBloc>();
+      await Future.delayed(const Duration(microseconds: 100));
+      accountBloc.add(const AccountLoad());
+      await Future.delayed(const Duration(microseconds: 500));
       _log.debug("redirect - accountBloc.state: ${accountBloc.state.status}");
       // check if the account is loaded
-      if (accountBloc.state.status == AccountStatus.initial) {
-        //
-        _log.debug("redirect with account load from initial");
-        accountBloc.add(const AccountLoad());
-        _log.debug("redirect with account load from initial - after add");
-        //
-        _log.debug("redirect with account load from initial - before delay");
-        await Future.delayed(const Duration(seconds: 1));
-        _log.debug("redirect with account load from initial - after delay");
-        //
-        if (accountBloc.state.status == AccountStatus.failure) {
-          _log.debug("END: redirect - ${accountBloc.state.status}  with login - initial>failure");
-          return ApplicationRoutesConstants.login;
-        }
+      // if (accountBloc.state.status == AccountStatus.initial) {
+      //   //
+      //   _log.debug("redirect with account load from initial");
+      //   accountBloc.add(const AccountLoad());
+      //   _log.debug("redirect with account load from initial - after add");
+      //   //
+      //   _log.debug("redirect with account load from initial - before delay");
+      //   await Future.delayed(const Duration(seconds: 1));
+      //   _log.debug("redirect with account load from initial - after delay");
+      //   //
+      //   if (accountBloc.state.status == AccountStatus.failure) {
+      //     _log.debug("END: redirect - ${accountBloc.state.status}  with login - initial>failure");
+      //     return ApplicationRoutesConstants.login;
+      //   }
+      // } else
+      if (accountBloc.state.status == AccountStatus.failure) {
+        _log.debug("END: redirect - ${accountBloc.state.status}  with login - initial>failure");
+        return ApplicationRoutesConstants.login;
       }
 
       _log.debug("END: redirect return null");
