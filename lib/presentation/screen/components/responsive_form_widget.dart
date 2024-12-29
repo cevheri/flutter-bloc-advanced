c76cba55a6959697a87f6938b32b73a875aebd3d
cevheri <cevheribozoglan@gmail.com>
2024-12-29 21:39:23 +0300

    refactor: add responsive form widget and responsive submit button for web or mobile

---
+++ lib/presentation/screen/components/responsive_form_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_advance/configuration/padding_spacing.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+
+class ResponsiveFormBuilder extends StatelessWidget {
+  final GlobalKey<FormBuilderState> formKey;
+  final List<Widget> children;
+  final MainAxisAlignment mainAxisAlignment;
+  final CrossAxisAlignment crossAxisAlignment;
+  final bool autovalidateMode;
+  final VoidCallback? onChanged;
+  final bool shrinkWrap;
+
+  const ResponsiveFormBuilder({
+    super.key,
+    required this.formKey,
+    required this.children,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+    this.crossAxisAlignment = CrossAxisAlignment.start,
+    this.autovalidateMode = false,
+    this.onChanged,
+    this.shrinkWrap = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FormBuilder(
+      key: formKey,
+      autovalidateMode: autovalidateMode ? AutovalidateMode.onUserInteraction : AutovalidateMode.disabled,
+      onChanged: onChanged,
+      child: SingleChildScrollView(
+        child: Center(
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: Spacing.formMaxWidthLarge),
+            child: Padding(
+              padding: const EdgeInsets.all(Spacing.medium),
+              child: Column(
+                spacing: Spacing.medium,
+                mainAxisAlignment: mainAxisAlignment,
+                crossAxisAlignment: crossAxisAlignment,
+                children: children,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/screen/components/submit_button_widget.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_advance/configuration/padding_spacing.dart';
+
+class ResponsiveSubmitButton extends StatelessWidget {
+  final String buttonText;
+  final VoidCallback? onPressed;
+  final bool isLoading;
+
+  const ResponsiveSubmitButton({super.key, this.buttonText = 'Save', this.onPressed, this.isLoading = false});
+
+  @override
+  Widget build(BuildContext context) {
+    final submitButton = FilledButton(
+      onPressed: isLoading ? null : onPressed,
+      child: ButtonContent(text: buttonText, isLoading: isLoading),
+    );
+
+    if (kIsWeb) {
+      return Align(alignment: Alignment.centerRight, child: submitButton);
+    }
+    return submitButton;
+  }
+}
+
+class ButtonContent extends StatelessWidget {
+  final String text;
+  final bool isLoading;
+
+  const ButtonContent({
+    super.key,
+    required this.text,
+    required this.isLoading,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final buttonForegroundColor = Theme.of(context).colorScheme.onPrimary;
+    return Row(
+      spacing: Spacing.small,
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        if (isLoading) ...[
+          SizedBox(
+            width: 16,
+            height: 16,
+            child: CircularProgressIndicator(
+              strokeWidth: 2,
+              valueColor: AlwaysStoppedAnimation<Color>(buttonForegroundColor),
+            ),
+          ),
+        ],
+        Text(text),
+      ],
+    );
+  }
+}
